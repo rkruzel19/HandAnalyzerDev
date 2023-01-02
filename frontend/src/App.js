6264f070b7480ca3a048ed02efc01efe54075a3f
Rob Kruzel <rkruzel576@gmail.com>
2023-01-02 16:38:01 -0500

    Created blank UserHompage files, forgot to create new branch

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserHomepage from './Pages/UserHompage/UserHomepage';
+
+function App() {
+  return (
+    <div className="App">
+      <UserHomepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/UserHompage/UserHomepage.js
@@ -0,0 +1,7 @@
+export default function UserHomepage(){
+    return (
+        <div>
+            <h1>UserHomepage World</h1>
+        </div>
+    )
+}
