fe6e2fc494a900d1fd689b5e945bf9c7bc68f3e8
Rob Kruzel <rkruzel576@gmail.com>
2023-01-02 18:29:31 -0500

    UserHomepage CSS. Laid out basic structure

--- frontend/src/Pages/UserHompage/UserHomepage.js
+++ frontend/src/Pages/UserHompage/UserHomepage.js
@@ -1,19 +1,32 @@
+import "./UserHomepage.css"
+
 export default function UserHomepage(){
     return (
         <div className="MainContainer">
             <header>
                 <div className="Logo">
-
+                    Logo goes here
                 </div>
                 <nav>
-
+                    Will be replaced with NavBar Component
                 </nav>
             </header>
-            <div className="AccountInfo">
-
-            </div>
-            <div className="MainArea">
-                
+            <div className="BodyArea">
+                <div className="AccountInfo">
+                    <ul>
+                        <li>User Info</li>
+                        <li>Stats</li>
+                        <li>Account Info</li>
+                    </ul>
+                </div>
+                <div className="MainArea">
+                    <div className="TournamentList">
+                        Tournament List Component
+                    </div>
+                    <div className="TournamentButtonPanel">
+                        Buttons for CRUD actions associated with Tournaments
+                    </div>
+                </div>
             </div>
         </div>
     )
