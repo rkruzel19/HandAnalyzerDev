f6b0e62a14c6bc2bea3b49a1858c916c86c3bd21
Rob Kruzel <rkruzel576@gmail.com>
2023-01-03 18:01:19 -0500

    Layed out basic CSS to get visual skeleton

--- frontend/src/Pages/welcomePage/WelcomePage.js
+++ frontend/src/Pages/welcomePage/WelcomePage.js
@@ -2,8 +2,18 @@ import "./WelcomePage.css"
 
 export default function WelcomePage(){
     return (
-        <div>
-            Welcome!!
+        <div className="Container">
+            <div className="WelcomeMessage">
+                Welcome Message
+            </div>
+            <div className="BodyArea">
+                <div className="DisplayField">
+                    Images and New User Form
+                </div>
+                <div className="LoginArea">
+                    Login here
+                </div>
+            </div>
         </div>
     )
 }
