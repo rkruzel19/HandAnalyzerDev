23658a7d3f4e75514df73d4688679a7653be6c3d
rkruzel19 <32852882+rkruzel19@users.noreply.github.com>
2023-01-03 19:03:59 -0500

    Feature/welcome page (#3)
    * Made WelcomePage and structured divs
    
    * Layed out basic CSS to get visual skeleton
    
    * Chose name and slogan for WelcomePage
    
    * Typo, forgot to save

---
+++ frontend/src/Pages/welcomePage/WelcomePage.js
@@ -0,0 +1,20 @@
+import "./WelcomePage.css"
+
+export default function WelcomePage(){
+    return (
+        <div className="Container">
+            <div className="WelcomeMessage">
+                WholeCard Poker<br/>
+                Take A Step Back To See The Bigger Picture
+            </div>
+            <div className="BodyArea">
+                <div className="DisplayField">
+                    Images and New User Form
+                </div>
+                <div className="LoginArea">
+                    Login here
+                </div>
+            </div>
+        </div>
+    )
+}
