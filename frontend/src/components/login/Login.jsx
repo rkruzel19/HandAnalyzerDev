2f9af10dcc692af2fb966bd9d1607f9bbeb04791
Rob Kruzel <rkruzel576@gmail.com>
2023-01-04 01:02:06 -0500

    Saving changes, switching to NewUser form until I watch authentication video.

---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,50 @@
+import "./Login.css"
+import { useState } from "react"
+import axios from "axios"
+
+export default function Login(){
+
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+
+    // function authenticateUser() {
+    //     axios.post('http://localhost:8080/user/login', {
+    //         userName: userName, 
+    //         password: password
+    //     }).then((response) =>{
+    //         const userData = response.data[0]
+    //         console.log(userData)
+    //     })
+    // }
+
+    return (
+        <div className="LoginContainer">
+            <article>
+                Enter account info below to Login or click Create Account to get started
+            </article>
+            <div className="UserCredentials">
+                <div className="username">
+                    <label>Username:</label>
+                    <input
+                        onChange = {(e) => setUserName(e.target.value)}
+                        name = "userName"
+                        type = "text"
+                        value = {userName}
+                    />
+                </div>
+                {userName}
+                <div className="password">
+                    <label>Password:</label>
+                    <input
+                        onChange = {(e) => setPassword(e.target.value)}
+                        name = "password"
+                        type = "text"
+                        value = {password}
+                    />
+                </div>
+            </div>
+            <button>Login</button>
+            <button>Create Account</button>
+        </div>
+    )
+}
---
+++ frontend/src/services/LoginService.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const BASE_URL = "http://localhost:8080/user/getUserById/1";
+
+class LoginService{
+
+    logUserIn(){
+        
+    }
+
+}
+
+export default new LoginService()
